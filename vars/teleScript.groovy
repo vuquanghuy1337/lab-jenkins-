fb95b93e7e508fe38ce031fa43ea05086d4b7b16
vuquanghuy1337 <vuquanghuy1337@gmail.com>
2022-09-12 02:14:04 +0000

    delete para

--- vars/teleScript.groovy
+++ vars/teleScript.groovy
@@ -1,11 +1,9 @@
 #!/usr/bin/env groovy
 
-def sendTelegramMessage(projectName, chatId, botToken, gitBranch) {
-  try {
+def call() {
     sh(
       script: """	#!/usr/bin/env bash
 	curl -s -X POST https://api.telegram.org/bot5700617525:AAGXbkqZ2psOplICMFSWVhggYM2ySrNtgeQ/sendMessage -d chat_id=-678535988 -d text="your message"
 	"""
-  }
 }
 
