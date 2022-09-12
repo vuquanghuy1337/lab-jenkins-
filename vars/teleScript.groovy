5a196567af36b5d093bd75c122b88a55b075c668
vuquanghuy1337 <vuquanghuy1337@gmail.com>
2022-09-12 02:35:06 +0000

    change script

--- vars/teleScript.groovy
+++ vars/teleScript.groovy
@@ -2,7 +2,7 @@
 
 def call() {
     sh(
-	/usr/bin/curl -s -X POST https://api.telegram.org/bot5700617525:AAGXbkqZ2psOplICMFSWVhggYM2ySrNtgeQ/sendMessage -d chat_id=-678535988 -d text="your message"
+	curl -s -X POST https://api.telegram.org/bot5700617525:AAGXbkqZ2psOplICMFSWVhggYM2ySrNtgeQ/sendMessage -d chat_id=-678535988 -d text="your message"
     )
 }
 
